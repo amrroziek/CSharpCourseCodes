1fd9ded59d890003f33342f0708d46c52b23cf6a
amrroziek <amrroziek@hotmail.com>
2018-04-21 01:55:20 +0400

    DataTypes-FloatPointType
    Thi is a program to explain the Float Point type and the differences between among them

---
+++ DataTypes-FloatPointType/DataTypes-FloatPointType/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes_FloatPointType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float flMin = float.MinValue;
+            float flMax = float.MaxValue;
+            Console.WriteLine("Min Value ={0} Max Value = {1} ",flMin,flMax);
+
+            float flVar = 1F / 3;
+            double dlvar = 1D / 3;
+            decimal dcVar = 1M / 3;
+
+            Console.WriteLine("\n FLoat Value = {0}\n Double Value = {1} \n Decimal Value ={2}", flVar, dlvar, dcVar);
+            Console.ReadKey();
+        }
+    }
+}
